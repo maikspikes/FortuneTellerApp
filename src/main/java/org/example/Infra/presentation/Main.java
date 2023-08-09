ff4ab2e1a863a3fc024ad47d60a6f699d04fdb17
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-09 09:28:47 +0200

    first commit

--- src/main/java/org/example/Infra/presentation/Main.java
+++ src/main/java/org/example/Infra/presentation/Main.java
@@ -1,7 +1,7 @@
 package org.example.Infra.presentation;
 
 public class Main {
-    public static void main(String[] args) {git
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
