1a5d65fc7f4aa2d73d9ee318f9561b52d981466c
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-09 11:12:47 +0200

    Revert "Revert "encapsulated the databse username and password better""
    This reverts commit c2721b16402870cdd65bc9431de7f3413bdbea20.

--- src/main/java/org/example/Infra/presentation/fortuneTeller.java
+++ src/main/java/org/example/Infra/presentation/fortuneTeller.java
@@ -9,15 +9,25 @@ import org.example.domain.Fortune;
 import spark.Spark;
 import spark.Spark.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class fortuneTeller {
     public static void main(String[] args) throws SQLException {
         Spark.get("/hello", (req, res) -> "Hello World");
 
-        String username = "fortune";
-        String password = "fortune";
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream("config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
 
         String databaseUrl = "jdbc:postgresql://localhost:5433/fortunetellerapp";
 
